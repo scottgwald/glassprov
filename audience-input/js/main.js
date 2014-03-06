1f02a2f0a54b3b877230e033383222615e232c65
Kevin Tu <kevin.b.tu@gmail.com>
2014-03-05 23:22:49 -0500

    newsroom doesn't work - but the rest should. Use enter, not submit button to sumbit ideas.

--- audience-input/js/main.js
+++ audience-input/js/main.js
@@ -1,26 +1,34 @@
 var results = new Array();
 
 function processInput(){
-  if(counter>6){
-    counter-=7;
+  if(counter>10){
+    counter-=11;
   }
   if(counter<0){
-    counter+=7;
+    counter+=11;
   }
+  defocus();
+  setTimeout(handleInput,200);
+  setTimeout(textFocus,500);
+} 
+function handleInput(){
 
   var inp = document.getElementsByClassName("txt")[counter].value;
   
   if(inp!=''){
     document.getElementsByClassName("txt")[counter].value ='';
-  var title = document.getElementsByClassName("title")[counter].innerHTML; 
+    var title = document.getElementsByClassName("title")[counter].innerHTML; 
     results.push([title,inp]);
-    alert("Submission Successful!"); 
+    //alert("Submission Successful!"); 
     //alert("Submission Successful!"+"\n"+results); 
-    defocus();
+    var right = document.getElementById('r');
+    right.click();
   } 
-} 
+}
  
-
+function textFocus(){
+  document.getElementsByClassName("txt")[counter].focus();
+}
 function defocus(){
   document.getElementsByClassName("txt")[counter].blur();
 }
