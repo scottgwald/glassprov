3c1b7d6f69f07ddb55244498341580bbb094b972
shri <shri@shri-VirtualBox.(none)>
2014-03-06 09:08:24 -0500

    moved functions to view

--- dashboard/dashboard/voting/models.py
+++ dashboard/dashboard/voting/models.py
@@ -1,45 +1,16 @@
 from django.db import models
 
-import random
-
 
 
 
 class Line(models.Model):
 	text = models.CharField(max_length=140)
 
-	def create(text):
-		return Line.objects.create(text=text)
-
-	def getLine(glassid):
-		line = Line.objects.all()[random.randint(0, Line.objects.count() - 1)]
-		text = line.text
-		line.delete()
-		return {"text":text, "glassid":glassid}
-
-
 class Emotion(models.Model):
 	text = models.CharField(max_length=140)
 	votes = models.IntegerField(default=1)
-
-	def create(text):
-		return Emotion.objects.create(text=text)
-
-	def getEmotion(glassid):
-		emotion = Emotion.objects.order_by('-votes')[0]
-		text = emotion.text
-		emotion.delete()
-		return {"text":text, "glassid":glassid}
 
 class Clip(models.Model):
 	text = models.CharField(max_length=140)
 	votes = models.IntegerField(default=1)
 
-	def create(text):
-		return Clip.objects.create(text=text)
-
-	def getClip(glassid):
-		clip = Clip.objects.order_by('-votes')[0]
-		text = clip.text
-		clip.delete()
-		return {"text":text, "glassid":glassid}
--- dashboard/dashboard/voting/views.py
+++ dashboard/dashboard/voting/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from .models import Line, Emotion, Clip
 
-# Create your views here.
+import random
+
+# lines in a hat
+
+def createLine(request):
+	text = request.POST["text"]
+	return Line.objects.create(text=text)
+
+def getLine(request):
+	line = Line.objects.all()[random.randint(0, Line.objects.count() - 1)]
+	text = line.text
+	line.delete()
+	return {"text":text, "glassid":request.GET["glassid"]}
+
+
+# jump styles
+
+def createEmotion(request):
+	text = request.POST["text"]
+	return Emotion.objects.create(text=text)
+
+def getEmotion(request):
+	emotion = Emotion.objects.order_by('-votes')[0]
+	text = emotion.text
+	emotion.delete()
+	return {"text":text, "glassid":request.GET["glassid"]}
+
+# news room
+
+def createClip(request):
+	text = request.POST["text"]
+	return Clip.objects.create(text=text)
+
+def getClip(request):
+	clip = Clip.objects.order_by('-votes')[0]
+	text = clip.text
+	clip.delete()
+	return {"text":text, "glassid":request.GET["glassid"]}
