9f3aad40725a8738f02b3c742467b67005ec62ce
Scott Greenwald <swgreen@mit.edu>
2014-07-22 05:09:59 -0400

    add glass id's prior to comedy central meeting

--- dashboard/dashboard/static/web-app/js/js.js
+++ dashboard/dashboard/static/web-app/js/js.js
@@ -68,12 +68,15 @@ var colorActor = invertKeyVal(actorColor);
 var idLookup = {
     'android:glass:fc4dd4cd5776': { color: "sky", colorIndex: "0", name: colorActor["sky"], works: true},
     'android:glass:fc4dd4cc51dc': { color: "sky", colorIndex: "1", name: colorActor["sky"], works: true},
+    'android:glass:f88fca25586b': { color: "sky", colorIndex: "2", name: colorActor["sky"], works: true},
     'android:glass:fc4dd4cd4fa6': { color: "charcoal", colorIndex: "0", name: colorActor["charcoal"], works: true},
     'android:glass:fc4dd4cdc3aa': { color: "charcoal", colorIndex: "1", name: colorActor["charcoal"], works: true},
+    'android:glass:f88fca261959': { color: "charcoal", colorIndex: "2", name: colorActor["charcoal"], works: true},
     'android:glass:fc4dd4cd59e8': { color: "cotton", colorIndex: "0", name: colorActor["cotton"], works: true},
     'android:glass:f88fca2506bf': { color: "tangerine", colorIndex: "0", name: colorActor["tangerine"]},
     'android:glass:fc4dd4cdb52a': { color: "tangerine", colorIndex: "1", name: colorActor["tangerine"], works: true},
     'android:glass:fc4dd4cb7b7e': { color: "tangerine", colorIndex: "2", name: colorActor["tangerine"], works: true},
+    'android:glass:f88fca25c50b': { color: "tangerine", colorIndex: "3", name: colorActor["tangerine"], works: true},
     'android:glass:f88fca26273d': { color: "shale", colorIndex: "0", name: colorActor["shale"]},
     'android:glass:f88fca2627c1': { color: "shale", colorIndex: "1", name: colorActor["shale"]}
 }
